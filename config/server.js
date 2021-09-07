46c785e6fdcc836e9fcc0a10d4037424d0d836a6
wfercanas <fernando.canas@outlook.com>
2021-09-07 12:02:45 -0500

    add/ first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c5723138fe75d028ffa95ba327ee85bb'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd1764a68-aff2-4abb-bac4-fe943022e80e'
+};
